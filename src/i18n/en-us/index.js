369018c518f7fde7cc358fba70c2bfc08fe6bc51
skiboorg <d@skib.org>
2022-02-04 11:03:45 +0300

    add slider game

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -92,78 +92,54 @@ export default {
   contacts_form_message:'Write here your message... ',
 
   rulesText:`
-<p><span lang="en-US">1. It is necessary to correctly assemble QR-ART Puzzle of one of three levels:</span></p>
-<p><span lang="en-US">- Choose one of the levels, click on it and get the elements scattered across the field.</span></p>
-<p><span lang="en-US">- Hover the cursor over any element, grab it and, while holding it, move it to any other element, if any of the sides of these elements coincide, then they will connect to each other themselves.</span></p>
-<p><span lang="en-US">- By the method of subsequent selection of elements, with the correct option, they all connect and form a single QR-ART Puzzle.</span></p>
-<p><span lang="en-US">- Each level has its own time to collect - the more difficult the level, the more time and points are given.</span></p>
-<p><span lang="en-US">- If you have collected the selected level correctly and on time, an inscription will appear on the playing field &ldquo;YOU WON&rdquo; and you will receive points in RATING, if you could not collect it in time, the inscription &ldquo;YOU LOSE&rdquo; will appear, then you have the opportunity to collect any level again ...</span></p>
-<p><span lang="en-US">It is imperative to watch the advertisement.</span></p>
-<p lang="en-US"></p>
-<p><span lang="en-US">2. All settlements are carried out in local currency - PZ (1 PZ = 1 YUAN), the funds transferred by you will be converted into PZ-currency, and when funds are withdrawn to your account, they will be converted back.</span></p>
-<p lang="en-US"></p>
-<p><span lang="en-US">3. Without registration:</span></p>
-<p><span lang="en-US">- you can train at all three levels in DEMO, but if you collect correctly, QR-ART Puzzle RATING and BALANCE will not be replenished.</span></p>
-<p lang="en-US"></p>
-<p><span lang="en-US">4. At registration, but without replenishment of BALANCE:</span></p>
-<p><span lang="en-US">- we give to your BALANCE 1000 YUAN = 1000 PZ, which you cannot withdraw, but you can transfer all the funds earned on them to your account if there are more than 200 points in RATING.</span></p>
-<p lang="en-US"></p>
-<p><span lang="en-US">5. When registering and replenishing BALANCE:</span></p>
-<p><span lang="en-US">- from 500 to 50,000 YUAN (from 70 to 7000 USD) for each correct collection of QR-ART Puzzle, your RATING will receive from 2 to 6 points, and upon reaching 200 points, your BALANCE will increase by 2%.</span></p>
-<p lang="en-US"><a name="_GoBack"></a> </p>
-<p><span lang="en-US">6. The number of fees is not limited, the number of points in RATING is not limited.</span></p>
-<p><span lang="en-US">Every 200 points in RATING = BALANCE + 2%</span></p>
-<p><span lang="en-US">The client can withdraw from BALANCE no more than 10,000 YUAN per day (24 hours), the requirement of the bank security service.</span></p>
+<p>1.I want to start play and earn! What do I need to do?</p>
+<ul>
+<li>
+<p>The first and probably the most important thing you will need to start playing is a Game Pass, which is our NFT.</p>
+</li>
+</ul>
+<p>2. Which NFT should I choose? What is the difference between them?</p>
+<ul>
+<li>
+<p>There are 3 types of Game Passes available for purchase: Bronze, Silver and Gold. The color of the pass determines the number of your games per day. If you get 3 and 7 games per day for a Bronze or Silver pass, the Gold pass gives you up to 15 games per day.</p>
+</li>
+</ul>
+<p>3.I bought a nft, what to do next?</p>
+<ul>
+<li>
+<p>To start you need to connect your game pass on our website, then you will get access. The player has several difficulty modes to choose from. The harder the task is, the higher the payment is.</p>
+</li>
+</ul>
+<p>4.How can I get my earnings?</p>
+<ul>
+<li>
+<p>Your earned money is automatically credited to your personal account. Withdrawal is available from 1.5 Sol at any time of day, the commission service for the withdrawal is 9.5%.</p>
+</li>
+</ul>
+<p>5. What if I passed all available tests?</p>
+<ul>
+<li>
+<p>Available tasks are updated every day at 00:00 UTC, so you can safely solve everything you think you can.</p>
+</li>
+</ul>
  `,
 
   aboutText:`
 
-<p>We have created and implemented the PuzPays puzzle, which allows you to explore the level of logical thinking among people of different nations, ages and genders.</p>
-<p>The goal of our project is to study the interaction of logical thinking with BNCI technologies and provide statistical data to marketing companies. We have focused our activities around a series of advanced hackathons with a program to support researchers and clients.</p>
-<p>PuzPays is a puzzle game that requires the ability to quickly solve logic puzzles. The program is an excellent stimulator for the development of logical thinking. All our work is analyzed, systematized and, on the basis of these data, a general picture is drawn up about the development of logical thinking in different regions and types of people.</p>
-<p>For us, this is a research activity, on the basis of which marketers, economists, manufacturers will draw conclusions about the advisability of introducing certain technologies in working with consumers.</p>
-<p>The logical thinking of people is the fundamental mechanism for choosing one or another product. Our PuzPays platform and analysis of its results simplifies the work of professionals in the field of marketing, promotion of goods and services. By selling this data to interested companies, we are able to attract large numbers of potential customers and encourage them to solve puzzles.</p>
+<p>We are a team of computer engineers from the university "Deutsches Forschungszentrum f&uuml;r K&uuml;nstliche Intelligenz" and our main focus is the training and development of AI.</p>
 
-<p>In our work, we use the company's CPA resource -  https://www.ga-net.com/ and its network. CPA ("cost per action" in translation from English "payment for action") is a way of making money in which the advertiser pays for some specific target user action. A CPA network is a platform where many advertisers are located who pay for a certain user action - purchase, registration, filling out questionnaires, and others.</p>
+<p>"DFKI is the leading, near-business research institution in Germany in the field of artificial intelligence with branches in Kaiserslautern, Saarbr&uuml;cken and Bremen.</p>
 
-<p>We offer our clients work in the form of PuzPays Puzzle solution, collecting statistical data and viewing advertisements. We are ready to pay for your work from the funds received for our activities.</p>
-<p>At first glance, everything will seem complicated, but with patience and experience, these tasks are solved and give us the opportunity to make good money together.</p>
-<p>Good luck!</p>
+<p>We launched this project with the support of Cyber Valley, whose partners include major companies such as Daimler, Bosch, Amazon, and Facebook. Thanks to this symbiosis, we have access to the most advanced technologies in the field of machine learning, and we can truly claim that our product is the first and unique of its kind</p>
 `,
   infoText:`
-<p><span lang="en-US">Questions to us:</span></p>
-<p><span lang="en-US"><strong>1. "Where did you get the funds to pay for Clients?"</strong></span></p>
-<p><span lang="en-US">- the collected statistical information about the work of our clients, we sell to interested companies,</span></p>
-<p><span lang="en-US">- advertising on our website, which you constantly watch, brings good income,</span></p>
-<p><span lang="en-US">- we produce Internet products that are highly profitable, we use the income from them for payments.</span></p>
-<p><span lang="en-US"><strong>2. "How much will I get if I get 200 points in the Rating, and how often can I do it?"</strong></span></p>
-<p><span lang="en-US">- every time you gain 200 points in the Rating, you will be automatically credited + 2% to the amount on your Balance,</span></p>
-<p><span lang="en-US">- You can do this as many times as you like, and every time, with a Rating of more than 200 points, + 2% will be credited.</span></p>
-<p><span lang="en-US"><strong>3. "Why should the Client refill the Balance?"</strong></span></p>
-<p><span lang="en-US">- to stimulate Clients to take more active actions on our website, the more often the Client comes to us, the more he watches advertising videos, and the more we have ad views, for which advertisers pay us</span></p>
-<p><span lang="en-US">- the more we are paid for advertising and for statistical data about Clients, the more we all earn</span></p>
-<p><span lang="en-US">- the greater the Client's Balance, the greater the amount of payments.</span></p>
-<p><span lang="en-US"><strong>4. "Guaranteed payments and safety of funds?"</strong></span></p>
-<p><a name="_GoBack"></a> <span lang="en-US">- it makes no sense for us to deceive anyone, tk. this will entail the closure of projects, loss of reputation and income,</span></p>
-<p><span lang="en-US">- we have enough working capital for the successful promotion of this resource,</span></p>
-<p><span lang="en-US">- our main goal is to develop this platform and have an income with you.</span></p>
-<p><span lang="en-US"><strong>5. "How to withdraw funds?"</strong></span></p>
-<p><span lang="en-US">- submit an application in your personal account for withdrawal of funds, the only requirement for the Client is that he should not withdraw more than 10,000 YUAN within a day (24 hours), this is a requirement of the bank's security service.</span></p>
-<p><span lang="en-US"><strong>6. "How many accounts can I open?"</strong></span></p>
-<p><span lang="en-US">- we have no restrictions on opening new accounts, we only limit the amount on each account, it must be from 500 to 50,000 YUAN (from 70 to 7000 USD), our 1000 YUAN gift boxes are not counted, you can only earn money on them.</span></p>
-<p><span lang="en-US"><strong>7. "How many fees do you need to get 200 points?"</strong></span></p>
-<p><span lang="en-US">- LEVEL EASY - 2 points per collection (200 points / 2) = 100 fees.</span></p>
-<p><span lang="en-US">- LEVEL MEDIUM - 4 points per collection (200 points / 4) = 50 fees.</span></p>
-<p><span lang="en-US">- LEVEL HARD - 6 points per collection (200 points / 6) = 34 fees.</span></p>
-<p><span lang="en-US">You can work at any Level and combine points. Everything in your Rating will be recorded and taken into account (see Personal Account).</span></p>
-<p><span lang="en-US"><strong>8. "I understood correctly that if I collect 200 points in the Rating per day and receive 2% each, then I can earn more than 50% on my Balance per month?"</strong></span></p>
-<p><span lang="en-US">- Yes, you calculated everything correctly, but forgot to mention that then in a month you will need to make about 1500 fees and spend more than 130 hours on it.
-According to our statistical observations, on average, our Client spends 2 hours of his time per day to work on our website, and this brings him about 25% per month.</span></p>
-<p><span lang="en-US"><strong>9. "Who manages the funds on my Balance?"</strong></span></p>
-<p><span lang="en-US">- only we can manage them, and then only in our common interests with you, namely: to develop this project, and constantly advertise it, so that many will learn about it and come to us. More customers, more opportunities for development, more advertising, more revenue from it.</span></p>
+<p>1.The main disadvantage of all the neural networks and artificial intelligence systems that exist today is that they are not capable of self-learning. <br />It is necessary to process gigabytes of data for a machine to understand how it should act in this or that situation.</p>
 
-<p><span lang="en-US">If you have any questions, write to us, and we will publish the most important ones</span><span style="font-family: Microsoft JhengHei, serif;"><span lang="en-US">!</span></span></p>
+<p>2. Using the experience of systems neurophysiology we have created a new model of computer interaction, which recognizes graphical tasks better than deep neural networks do</p>
 
+<p>3. The program is based on a new hybrid technology combining the technologies of hierarchical temporal memory and Recursive Cortical Network. These are special kinds of neural networks, capable of imitating the work of the cerebral cortex, and it is through this symbiosis that new methods of learning and further self-learning became available to us</p>
+
+<p>4. The "mind" of our AI is based on the SingularityNET blockchain. SingularityNet is an open platform for creating a decentralized market for AI services. It is thanks to the SingularityNet blockchain that each participant of our project is a direct "teacher" for our AI, since each module is trained independently of the other, each user actually has his own RoboTutor. The famous humanoid "Sophia" is based on the same blockchain</p>
 `
   ,
 }
